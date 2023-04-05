81f820038e0115d1acadbe86089e85a07e7b11c3
Michał Szczurek <64778378+szczursonn@users.noreply.github.com>
2023-04-05 11:52:20 +0000

    show warning if part of production is ignored

--- utils.mjs
+++ utils.mjs
@@ -43,7 +43,8 @@ export const LABELS = {
     PLANNED_ORDERS_INTAKE: 'Planowane przyjęcie zamówień',
     BACK: 'Powrót',
     INPUT_FORM_TITLE: 'Wprowadź dane',
-    HIDE_ZEROES: 'Ukryj zera'
+    HIDE_ZEROES: 'Ukryj zera',
+    IGNORED_PRODUCTION_WARNING: 'Uwaga: Wprowadzona produkcja musiałaby zacząć się przed pierwszym okresem. Zostanie ona zignorowana.'
 }
 
 export const TEST_PARAMS = {
